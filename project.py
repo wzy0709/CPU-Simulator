c1468f33677c7fe067401b15c2d11cb00f955af5
wangz45@rpi.edu <wangz45@rpi.edu>
2021-07-30 13:07:00 +0800

    rr added

--- project.py
+++ project.py
@@ -1,9 +1,6 @@
-import queue
 import sys
 import math
 import copy
-from queue import Queue
-import bisect 
 
 # rand function
 class Rand48( object ):
@@ -26,6 +23,7 @@ def next_exp():
 class CPU (object):
     def __init__(self, cpu_time, io_time):
         self.cpu_time = cpu_time
+        self.remain = cpu_time
         self.io_time = io_time
 # process class
 class Process( object ):
@@ -35,6 +33,7 @@ class Process( object ):
         self.bursts = []
         self.cw_time = 0
         self.io_time = 0
+        self.add_time = 0
 # create processor
 def create_process(n):
     process = Process(n, math.floor(next_exp()))
@@ -51,117 +50,51 @@ def create_process(n):
 
 # get string
 def get_string(processes):
-    mystr = "[Q"
+    mystr = "[Q "
     if not processes:
-        mystr += " empty"
+        mystr += "empty"
     else:
         for p in processes:
-            mystr+=' '
             mystr+=p.name
     mystr += ']'
     return mystr
-#FCFS algo
-def fcfs(processes):
-    current_time = 0
-    cpu_total_time = 0
-    wait_time = 0
-    turnaround = 0
-    cpu_working = False
-    cpu_time = 0
-    cpu_p = None
-    io_c = None
-    io_p = None
-    cpu_q = []
-    io_q = []
-    cs_count = 0
-    print("time %ims: Simulator started for FCFS %s"% (current_time, get_string(cpu_q) ) )
-    psort = sorted(processes,key=lambda x:x.arrival_time)
-    while 1:
-        # new process arrrives
-        if psort and current_time==psort[0].arrival_time:
-            new_p = psort.pop(0)
-            # add to queue
-            new_p.cw_time = current_time + context_switch_time/2
-            cpu_q.append(new_p)
-            if current_time<1000:
-                print("time %ims: Process %c arrived; added to ready queue %s"% (current_time, new_p.name, get_string(cpu_q) ) )
-        current_time+=1
-        # cpu process
-        if not cpu_working:
-            if len(cpu_q)>1:
-                wait_time += (len(cpu_q)-1)
-            # context switch satisfies
-            if cpu_q and current_time >= cpu_q[0].cw_time:
-                # start process the process
-                cpu_p = cpu_q.pop(0)
-                c = cpu_p.bursts[0]
-                cs_count+=1
-                cpu_total_time += c.cpu_time
-                if current_time<1000:
-                    print("time %ims: Process %c started using the CPU for %ims burst %s"% 
-                        (current_time, cpu_p.name, c.cpu_time, get_string(cpu_q) ))
-                cpu_time = current_time+c.cpu_time
-                cpu_working = True
-        # check if cpu finishes
-        else:
-            # cpu finishes
-            wait_time += len(cpu_q)
-            if current_time== cpu_time:
-                cpu_p.cw_time = current_time + context_switch_time/2
-                io_c = cpu_p.bursts.pop(0)
-                # process finishes
-                if len(cpu_p.bursts) == 0:
-                    print("time %ims: Process %c terminated %s"%
-                        (current_time, cpu_p.name, get_string(cpu_q) ) )
-                    turnaround+=(current_time-cpu_p.arrival_time)
-                else:
-                    # go to i/o
-                    io_p = cpu_p
-                    io_p.io_time = current_time + io_c.io_time + context_switch_time/2
-                    io_q.append(io_p)
-                    io_q.sort(key = lambda x:(x.io_time,x.name))
-                    if current_time<1000:
-                        if len(cpu_p.bursts)==1:
-                            print("time %ims: Process %c completed a CPU burst; %i burst to go %s"% 
-                                (current_time, cpu_p.name, len(cpu_p.bursts), get_string(cpu_q) ) )
-                        else:
-                            print("time %ims: Process %c completed a CPU burst; %i bursts to go %s"% 
-                                (current_time, cpu_p.name, len(cpu_p.bursts), get_string(cpu_q) ) )
-                        print("time %ims: Process %c switching out of CPU; will block on I/O until time %ims %s"% 
-                            (current_time, io_p.name, io_p.io_time , get_string(cpu_q) ) )
-            # switch finishes
-            elif current_time==cpu_p.cw_time:
-                cpu_working = False
-                if len(cpu_p.bursts) == 0 and not cpu_q and not io_q:
-                    #finishes
-                    print("time %ims: Simulator ended for FCFS [Q empty]"%(current_time))
-                    return [current_time, cpu_total_time, cs_count, wait_time, turnaround]
-                elif cpu_q:
-                    cpu_q[0].cw_time = current_time + context_switch_time/2
-        #i/o finishes
-        while io_q and (current_time==io_q[0].io_time):
-            io_p = io_q.pop(0)
-            io_p.cw_time = current_time + context_switch_time/2
-            cpu_q.append(io_p)
-            if  current_time<1000:
-                print("time %ims: Process %c completed I/O; added to ready queue %s"% 
-                    (current_time, io_p.name, get_string(cpu_q) ))
 
 #RR algo
-def rr(processes):
+def RR(processes, fcfs):
+    slice_time = 0
+    # which method to use
+    if fcfs:
+        slice_time = sys.maxsize
+    else:
+        slice_time = tslice
+    # if print <1000
+    printime = 1000
+    # total time spent
     current_time = 0
-    cpu_total_time = 0
+    # wait time
     wait_time = 0
-    turnaround = 0
+    # if cpu is working
     cpu_working = False
+    # how long the cpu would be working
     cpu_time = 0
+    # current cpu process
     cpu_p = None
+    # current i/o burst
     io_c = None
+    # currrent i/o process
     io_p = None
+    # cpu queue
     cpu_q = []
+    # i/o queue
     io_q = []
+    # context switch count
     cs_count = 0
-    print("time %ims: Simulator started for FCFS %s"% (current_time, get_string(cpu_q) ) )
+    # premption count
+    pre_count = 0
+    if fcfs:
+        print("time %ims: Simulator started for FCFS %s"% (current_time, get_string(cpu_q) ) )
+    else:
+        print("time %ims: Simulator started for RR with time slice %ims [Q empty]"% (current_time, slice_time) )
     psort = sorted(processes,key=lambda x:x.arrival_time)
     while 1:
         # new process arrrives
@@ -170,7 +103,7 @@ def rr(processes):
             # add to queue
             new_p.cw_time = current_time + context_switch_time/2
             cpu_q.append(new_p)
-            if current_time<1000:
+            if current_time<printime:
                 print("time %ims: Process %c arrived; added to ready queue %s"% (current_time, new_p.name, get_string(cpu_q) ) )
         current_time+=1
         # cpu process
@@ -183,16 +116,22 @@ def rr(processes):
                 cpu_p = cpu_q.pop(0)
                 c = cpu_p.bursts[0]
                 cs_count+=1
-                cpu_total_time += c.cpu_time
-                if current_time<1000:
-                    print("time %ims: Process %c started using the CPU for %ims burst %s"% 
-                        (current_time, cpu_p.name, c.cpu_time, get_string(cpu_q) ))
-                cpu_time = current_time+c.cpu_time
+                stop_time = current_time+slice_time
+                if current_time<printime:
+                    # start
+                    if c.remain==c.cpu_time:
+                        print("time %ims: Process %c started using the CPU for %ims burst %s"% 
+                            (current_time, cpu_p.name, c.remain, get_string(cpu_q) ))
+                    # preempt
+                    else:
+                        print("time %ims: Process %c started using the CPU for remaining %ims of %ims burst %s"%
+                                (current_time, cpu_p.name, c.remain, c.cpu_time, get_string(cpu_q) ) )
+                cpu_time = current_time+c.remain
                 cpu_working = True
         # check if cpu finishes
         else:
-            # cpu finishes
             wait_time += len(cpu_q)
+            # cpu finishes
             if current_time== cpu_time:
                 cpu_p.cw_time = current_time + context_switch_time/2
                 io_c = cpu_p.bursts.pop(0)
@@ -200,14 +139,13 @@ def rr(processes):
                 if len(cpu_p.bursts) == 0:
                     print("time %ims: Process %c terminated %s"%
                         (current_time, cpu_p.name, get_string(cpu_q) ) )
-                    turnaround+=(current_time-cpu_p.arrival_time)
                 else:
                     # go to i/o
                     io_p = cpu_p
                     io_p.io_time = current_time + io_c.io_time + context_switch_time/2
                     io_q.append(io_p)
                     io_q.sort(key = lambda x:(x.io_time,x.name))
-                    if current_time<1000:
+                    if current_time<printime:
                         if len(cpu_p.bursts)==1:
                             print("time %ims: Process %c completed a CPU burst; %i burst to go %s"% 
                                 (current_time, cpu_p.name, len(cpu_p.bursts), get_string(cpu_q) ) )
@@ -217,31 +155,63 @@ def rr(processes):
                         print("time %ims: Process %c switching out of CPU; will block on I/O until time %ims %s"% 
                             (current_time, io_p.name, io_p.io_time , get_string(cpu_q) ) )
             # switch finishes
-            elif current_time==cpu_p.cw_time:
+            if current_time==cpu_p.cw_time:
                 cpu_working = False
                 if len(cpu_p.bursts) == 0 and not cpu_q and not io_q:
                     #finishes
-                    print("time %ims: Simulator ended for FCFS [Q empty]"%(current_time))
-                    return [current_time, cpu_total_time, cs_count, wait_time, turnaround]
+                    if fcfs:
+                        print("time %ims: Simulator ended for FCFS [Q empty]"%(current_time))
+                    else:
+                        print("time %ims: Simulator ended for RR [Q empty]"%(current_time))
+                    return [current_time, cs_count, wait_time, pre_count]
                 elif cpu_q:
                     cpu_q[0].cw_time = current_time + context_switch_time/2
+            #not finishes but need to stop
+            elif current_time<cpu_time and current_time==stop_time:
+                c.remain = c.remain -slice_time
+                if cpu_q:
+                    c = cpu_p.bursts[0]
+                    # preempt the cpu
+                    pre_count += 1
+                    cpu_p.add_time = current_time + context_switch_time/2
+                    cpu_time = sys.maxsize
+                    if current_time<printime:
+                        print("time %ims: Time slice expired; process %c preempted with %ims to go %s"%
+                            (current_time, cpu_p.name,c.remain, get_string(cpu_q) ) )
+                else:
+                    stop_time = current_time + slice_time
+                    if current_time<printime:
+                        print("time %ims: Time slice expired; no preemption because ready queue is empty %s"%
+                            (current_time, get_string(cpu_q)) )
+            # sw satisfies
+            if current_time == cpu_p.add_time:
+                cpu_working = False
+                cpu_p.cw_time = current_time + context_switch_time/2
+                cpu_q[0].cw_time = current_time + context_switch_time/2
+                cpu_q.append(cpu_p)
         #i/o finishes
         while io_q and (current_time==io_q[0].io_time):
             io_p = io_q.pop(0)
             io_p.cw_time = current_time + context_switch_time/2
             cpu_q.append(io_p)
-            if  current_time<1000:
+            if  current_time<printime:
                 print("time %ims: Process %c completed I/O; added to ready queue %s"% 
                     (current_time, io_p.name, get_string(cpu_q) ))
 
 # output
-def output(ret, processes):
-    [total_time, cpu_time, cs_count, wait_time, turnaround] = ret
-    output = 'Algorithm FCFS\n'
-    turnaround = cpu_time+ (cs_count*context_switch_time)+wait_time
+def output(ret, processes, fcfs):
+    [total_time, cs_count, wait_time, pre_count] = ret
+    if fcfs:
+        output = 'Algorithm FCFS\n'
+    else:
+        output = 'Algorithm RR\n'
     bur_count = 0
+    cpu_time = 0
     for p in processes:
         bur_count+=len(p.bursts)
+        for c in p.bursts:
+            cpu_time += c.cpu_time
+    turnaround = cpu_time+ (cs_count*context_switch_time)+wait_time
     line = "-- average CPU burst time: %.3f ms\n"%(cpu_time/bur_count)
     output+=line
     line = "-- average wait time: %.3f ms\n"%(wait_time/bur_count)
@@ -250,7 +220,7 @@ def output(ret, processes):
     output+=line
     line = "-- total number of context switches: %i\n"%(cs_count)
     output+=line
-    line = "-- total number of preemptions: 0\n"
+    line = "-- total number of preemptions: %i\n" %(pre_count)
     output+=line
     line = "-- CPU utilization: %.3f"%((cpu_time/total_time)*100)
     line += '%\n'
@@ -271,6 +241,7 @@ if ( __name__ == "__main__" ):
     global max_bound
     global lam
     global context_switch_time
+    global tslice
     num_process = 16
     seedval = 2
     lam = 0.01
@@ -287,10 +258,11 @@ if ( __name__ == "__main__" ):
         print("Process %c (arrival time %i ms) %i CPU bursts (tau %ims)" %
                 (processes[i].name, processes[i].arrival_time, len(processes[i].bursts), 1/lam))
     print('')
-    file = open("simout.txt","a+")
-    ret = fcfs(copy.deepcopy(processes))
-    file.write(output(ret, processes))
-    ret = rr(copy.deepcopy(processes))
-    file.write(output(ret, processes))
+    file = open("simout.txt","w")
+    ret = RR(copy.deepcopy(processes), True)
+    file.write(output(ret, processes, True))
+    print('')
+    ret = RR(copy.deepcopy(processes), False)
+    file.write(output(ret, processes, False))
     file.close()
     
